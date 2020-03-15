788c39a46bc385fe793d92bd5bbbede1d736bc5b
amoljgit <amol.joshi1984@gmail.com>
2020-03-16 02:05:59 +0530

    Upated NewTest.java

--- src/test/java/com/mvn/demo/NewTest.java
+++ src/test/java/com/mvn/demo/NewTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class NewTest {
@@ -17,22 +18,39 @@ public class NewTest {
 		driver = new ChromeDriver();
 
 		driver.get("file:///D:/Offline%20Website/index.html");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
 
 	}
 
 	@Test
-	public void f() {
+	public void TestLogin() throws InterruptedException {
 
 		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("123456");
 		driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();
+		
 	}
 
 	@Test
-	public void test2() {
+	public void TestGettitle() {
 		String title = driver.getTitle();
-		Assert.assertEquals("JavaByKiran | Dashboard", title);
+		try {
+			Assert.assertEquals("JavaByKiran | Dashboard", title);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Page title is not matching, test case failed.");
+		}
 
+	}
+	
+	@AfterSuite
+	public void TearDown() {
+		
+		driver.findElement(By.linkText("LOGOUT"));
+		driver.close();
+		driver.quit();
 	}
 
 }
